ef8689a23611c6372e996352a3a78b5f6400a4eb
tinue619 <_email@gmail.com>
2025-11-05 12:33:52 +0500

    feat: add drawer recalculation on cabinet depth change
    - Added drawer.updateParts() call in updateCabinetDepth() function
    - Exported updateDrawerMeshes to window scope in main.js
    - Ensures drawer dimensions are recalculated when cabinet depth changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { App } from './App.js';
+import { updateDrawerMeshes } from './modules/render3D.js';
 
 // ========== ЗАПУСК ==========
 const app = new App();
@@ -6,3 +7,4 @@ app.init();
 
 // Экспортируем для доступа из консоли браузера (для отладки)
 window.cabinetApp = app;
+window.updateDrawerMeshes = updateDrawerMeshes;
